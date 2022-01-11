f23b18b33e0287db49fb164a0f90f87e0b96a76a
Jackie Yang <jackie@jackieyang.me>
2022-01-11 03:45:42 -0800

    new enhanced script 2 using python generator

--- simulate.py
+++ simulate.py
@@ -1,6 +1,9 @@
+import time
+
 from unity_simulator.comm_unity import UnityCommunication
 
 comm = UnityCommunication()
+
 script_1 = [
     '<char0> [sit] <chair> (392)',
     '<char0> [open] <door> (47)',
@@ -9,7 +12,6 @@ script_1 = [
     '<char0> [switchoff] <tablelamp> (377)',
     '<char0> [open] <microwave> (172)'
 ]  # Add here your script
-
 
 script_2 = [
     '<char0> [switchon] <tablelamp> (377)',
@@ -38,8 +40,15 @@ script_3 = [
     '<char0> [open] <fridge> (163)']  # Add here your script
 
 # 5 lights, 3 doors, 3 lamps
-local_lookup_table = [58, 239, 278, 344, 402, 47, 254, 305, 256, 376, 377]
-
+local_light_lookup_table = [58, 239, 278, 344, 402]
+local_light_name_table = [f"Light {i}" for i in range(len(local_light_lookup_table))]
+local_door_lookup_table = [47, 254, 305]
+local_door_name_table = [f"Door {i}" for i in range(len(local_door_lookup_table))]
+local_lamp_lookup_table = [256, 376, 377]
+local_lamp_name_table = [f"Lamp {i}" for i in range(len(local_lamp_lookup_table))]
+local_lookup_table = local_light_lookup_table + local_door_lookup_table + local_lamp_lookup_table
+local_name_table = local_light_name_table + local_door_name_table + local_light_name_table
+local_name_lookup_table = {i: j for i, j in zip(local_lookup_table, local_name_table)}
 
 def find_nodes(graph, **kwargs):
     if len(kwargs) == 0:
@@ -79,7 +88,7 @@ class Processor:
             return graph
 
     def initialize_graph_task_2(self):
-        self.local_states_table = [1] * 5 + [0, 1, 1] + [0] * 3
+        self.local_states_table = [1] * 5 + [1, 1, 1] + [0] * 3
         if not self.get_all_history:
             comm.reset()
             success, graph = comm.environment_graph()
@@ -172,6 +181,12 @@ class Processor:
         # else_action = ["1 0"]
 
         for action in script:
+            if action.startswith('!'):
+                if action.startswith('!print'):
+                    comm.experiment_log(action[7:])
+                else:
+                    print("invalid action!!!")
+                continue
             if trigger and trigger in action:
                 self.my_render_script(action)
                 # _, graph = get_current_states()
@@ -278,6 +293,80 @@ class Processor:
         return light_states + door_states + tablelamp_states, graph
 
 
+def enhanced_script_2(my_p: Processor):
+    comm.setup_experiment_log()
+    manual = 0
+    yield '!print One day:'
+    time.sleep(1)
+    yield '!print It\'s late now. I need to go to sleep...'
+    time.sleep(1)
+    yield from [
+        f'!print I\'ll turn on my bed side lamp ({local_lamp_name_table[2]}).',
+        f'<char0> [walk] <door> (47)',
+        f'<char0> [open] <door> (47)',
+        f'<char0> [switchon] <tablelamp> ({local_lamp_lookup_table[2]})',
+        f'<char0> [walk] <door> (47)',
+        f'<char0> [close] <door> (47)',
+        '<char0> [switchoff] <light> (402)',
+    ]
+    yield from [
+        '!print OK. I\'ll go to bed now.',
+        '<char0> [sit] <bed> (394)',
+    ]
+    time.sleep(1)
+    on_light_ids = [
+        local_light_lookup_table[light_idx]
+        for light_idx in range(5)
+        if my_p.local_states_table[light_idx] != 0
+    ]
+    if len(on_light_ids) != 0:
+        # If any lights is still on, the user opens the door and then turns off those lights.
+        yield from [
+            '!print OHHHH. I forgot to turn off other lights again!',
+            '<char0> [switchon] <light> (402)',
+            '<char0> [open] <door> (47)'
+        ]
+        for light_id in on_light_ids:
+            manual += 1
+            yield f'!print I have to MANUALLY TURN OFF {local_name_lookup_table[light_id]}!'
+            yield f'<char0> [switchoff] <light> ({light_id})'
+        yield from [
+            '!print I can finally go to sleep now. I wish it\'s all automatic!',
+            '<char0> [switchoff] <light> (402)',
+            f'<char0> [walk] <bed> (394)',
+            '<char0> [sit] <bed> (394)',
+        ]
+    time.sleep(1)
+    yield '!print Sleeping...zzz...'
+    time.sleep(1)
+    yield '!print Next morning:'
+    yield from [
+        '!print What a nice day!',
+        '!print I\'ll go to cooking something the living room',
+        f'!print First, I\'ll turn off my bed side lamp ({local_lamp_lookup_table[2]}).',
+        f'<char0> [switchon] <light> ({local_light_lookup_table[4]})',
+        f'<char0> [switchoff] <tablelamp> ({local_lamp_lookup_table[2]})',
+        '<char0> [open] <door> (47)',
+        f'<char0> [switchon] <light> ({local_light_lookup_table[0]})',
+        f'<char0> [switchoff] <light> ({local_light_lookup_table[4]})',
+        '<char0> [close] <door> (47)',
+    ]
+    if my_p.local_states_table[0] == 0:
+        manual += 1
+        yield from [
+            '!print WHAT!!! Why is my light off now???',
+            f'!print I have to MANUALLY TURN ON {local_light_lookup_table[0]}!',
+            f'<char0> [switchon] <light> ({local_light_lookup_table[0]})',
+        ]
+    yield from [
+        '<char0> [open] <microwave> (172)',
+        '!print end of story',
+        f'!print Summary: I have to do stuff manually {manual} time(s)'
+        if manual > 0 else
+        f'!print Summary: I don\'t have do things manually ;)'
+    ]
+
+
 def sim_in_unity(selected_task, input, get_all_history=False):
     my_p = Processor(get_all_history)
     print(input)
@@ -299,8 +388,8 @@ def sim_in_unity(selected_task, input, get_all_history=False):
         else:
             graph = my_p.initialize_graph_task_2()
             _ = comm.expand_scene(graph)
-            comm.add_character('Chars/Female1', initial_room="bedroom")
-            my_p.process_programm(script_2, input)
+            comm.add_character('Chars/Female1', initial_room="livingroom")
+            my_p.process_programm(enhanced_script_2(my_p), input)
     else:
         ##### Task 3
         if get_all_history:
